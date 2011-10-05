61e3b624212642a6fc1c2b432af0cf1c143c3a38
aleksander.kmetec@gmail.com <aleksander.kmetec@gmail.com>
2011-10-05 20:53:55 +0200

    Support for command-line parameters. Also added package.json with deps.

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 
+var argv = require('optimist').argv;
+var config = require('./config/config.defaults.js');
+
+if (argv['h'])         { config.serverHost        = argv['h']; } 
+if (argv['host'])      { config.serverHost        = argv['host']; }
+
+if (argv['p'])         { config.serverPort        = argv['p']; } 
+if (argv['port'])      { config.serverPort        = argv['port']; } 
+
+if (argv['d'])         { config.fileServerBaseDir = argv['d']; } 
+if (argv['file-dir'])  { config.fileServerBaseDir = argv['file-dir']; } 
+
+if (argv['file-port']) { config.fileServerPort    = argv['file-port']; }
+
+
 var server = require('./server/server.js');
 var debugFileServer = require('./server/debug-file-server.js');
 
